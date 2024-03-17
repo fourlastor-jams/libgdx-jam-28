815d87561bd3cea1695247c8391f363800f99eb6
Daniele Conti <fourlastor@users.noreply.github.com>
2024-03-17 11:09:37 +0100

    Use different colors for segments

--- core/src/main/java/io/github/fourlastor/game/intro/RoadDrawable.java
+++ core/src/main/java/io/github/fourlastor/game/intro/RoadDrawable.java
@@ -1,5 +1,7 @@
 package io.github.fourlastor.game.intro;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
@@ -8,26 +10,36 @@ import space.earlygrey.shapedrawer.scene2d.ShapeDrawerDrawable;
 
 class RoadDrawable extends ShapeDrawerDrawable {
 
+    private static final Color[] COLORS = new Color[] {
+        new Color(Color.RED).lerp(Color.GRAY, 0.5f),
+        new Color(Color.GREEN).lerp(Color.GRAY, 0.5f),
+        new Color(Color.BLUE).lerp(Color.GRAY, 0.5f),
+        new Color(Color.YELLOW).lerp(Color.GRAY, 0.5f),
+        new Color(Color.ORANGE).lerp(Color.GRAY, 0.5f),
+        new Color(Color.PINK).lerp(Color.GRAY, 0.5f),
+        new Color(Color.PURPLE).lerp(Color.GRAY, 0.5f)
+    };
+
     private static final int CAM_X = 0;
 
     /**
      * Height from the floor
      */
-    private static final int CAM_Y = 150;
+    private static final int CAM_Y = 180;
 
     /** How far along the road. */
-    private float camZ = -1000;
+    private float camZ = 0;
 
     /**
      * Camera distance from screen
      */
-    private static final int CAMERA_DEPTH = 40;
+    private static final int CAMERA_DEPTH = 10;
 
     /** Length of a single segment. */
-    private static final int SEGMENT_LENGTH = 1000;
+    private static final int SEGMENT_LENGTH = 500;
 
     /** How many road segments */
-    private static final int SEGMENTS_COUNT = 10;
+    private static final int SEGMENTS_COUNT = 100;
 
     /** How many segments form a single patch of road. */
     private static final int RUMBLE_LENGTH = 1;
@@ -39,33 +51,33 @@ class RoadDrawable extends ShapeDrawerDrawable {
         segments = new Array<>(SEGMENTS_COUNT);
         for (int i = 0; i < SEGMENTS_COUNT; i++) {
             int segmentLength = SEGMENT_LENGTH;
-            Segment segment = new Segment(
-                    i * segmentLength,
-                    (i + 1) * segmentLength,
-                    (i / RUMBLE_LENGTH) % 2 == 0 ? Color.GRAY : Color.LIGHT_GRAY);
+            Color color = COLORS[(i / RUMBLE_LENGTH) % COLORS.length];
+            Segment segment = new Segment(i * segmentLength, (i + 1) * segmentLength, color);
             segments.add(segment);
         }
     }
 
     private int findSegmentIndex(float z) {
-        return MathUtils.ceil(z / SEGMENT_LENGTH) % segments.size;
+        return Math.max(0, MathUtils.ceil(z / SEGMENT_LENGTH) % segments.size);
     }
 
     @Override
     public void drawShapes(ShapeDrawer shapeDrawer, float x, float y, float width, float height) {
-        //            int segmentIndex = findSegmentIndex(camZ);
-        //            for (int i = segmentIndex; i < segments.size && i - segments.size < 10; i++) {
-        //                Segment segment = segments.get(i);
-        //                segment.project(0, 5, camZ, 250, width, height, width / 4, x + width / 2, y + width / 2);
-        //                segment.render(shapeDrawer);
-        //            }
-        for (Segment segment : segments) {
-            segment.project(CAM_X, CAM_Y, camZ, CAMERA_DEPTH, width, height, width / 4);
+        int segmentIndex = findSegmentIndex(camZ);
+        for (int i = segmentIndex; i < segments.size && i - segments.size < 10; i++) {
+            Segment segment = segments.get(i);
+            segment.project(CAM_X, CAM_Y, camZ, CAMERA_DEPTH, width, height, width / 2);
             segment.render(shapeDrawer);
         }
         //            camZ += 300;
         //            if (camZ >= 100000) {
         //                camZ = 0;
         //            }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            camZ -= 100;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            camZ += 100;
+        }
     }
 }
