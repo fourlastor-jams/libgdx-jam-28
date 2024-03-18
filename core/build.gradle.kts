bad165810bcb12982c0aba597b2bbb91c006b707
Daniele Conti <fourlastor@users.noreply.github.com>
2024-03-18 19:13:49 +0100

    Add sources to html module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(libs.gdx.core)
     implementation(libs.gdx.ai)
     implementation(libs.ashley)
-    implementation(libs.shapedrawer)
+    implementation(libs.earlgray.shapedrawer)
     implementation(libs.tommyettinger.textratypist)
     implementation(libs.tommyettinger.jdkgdxds)
     implementation(libs.dagger.core)
